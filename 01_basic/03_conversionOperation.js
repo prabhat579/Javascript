831dd274b6975707cf4925e6280895f41c64bca4
Prabhat <prabhat.mindnerves@gmail.com>
2024-04-04 00:15:59 +0530

    conversion1

---
+++ 01_basic/03_conversionOperation.js
@@ -0,0 +1,28 @@
+let score = "33a"
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber =  Number(score)
+
+console.log(valueInNumber);
+
+// "33" = 33
+// "abc" = NaN
+
+isLoggedIn = 1;
+
+booleanLoggedIn = Boolean(isLoggedIn)
+
+console.log(booleanLoggedIn);
+
+// "hitesh" = true
+//  '' = false
+//  '0' = true
+
+
+let someNumber = "33";
+
+converString = String(someNumber)
+
+console.log(typeof(converString));
