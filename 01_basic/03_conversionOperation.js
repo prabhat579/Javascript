c91d642e86d2c61d94eac4c5503c9c0df8e8edf3
Prabhat <prabhat.mindnerves@gmail.com>
2024-04-04 23:27:17 +0530

    opration

--- 01_basic/03_conversionOperation.js
+++ 01_basic/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score = "33a"
 
-console.log(typeof score)
-console.log(typeof(score));
+// console.log(typeof score)
+// console.log(typeof(score));
 
 let valueInNumber =  Number(score)
 
-console.log(valueInNumber);
+// console.log(valueInNumber);
 
 // "33" = 33
 // "abc" = NaN
@@ -14,7 +14,7 @@ isLoggedIn = 1;
 
 booleanLoggedIn = Boolean(isLoggedIn)
 
-console.log(booleanLoggedIn);
+// console.log(booleanLoggedIn);
 
 // "hitesh" = true
 //  '' = false
@@ -25,4 +25,33 @@ let someNumber = "33";
 
 converString = String(someNumber)
 
-console.log(typeof(converString));
+// console.log(typeof(converString));
+
+// ************************Opration*******************************
+
+let value = 3;
+
+let negvalue = -value
+
+// console.log(negvalue); // -3
+// console.log(2%3);
+
+
+let str1 = "hello"
+let str2 = " Prabhat"
+
+let str3 = str1 + str2
+
+// console.log(str3); // hello Prabhat
+// console.log("1" + 2); // 12
+// console.log(1 + "2"); // 12
+// console.log("1" + 2 + 2); // 122
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(+true); // 1
+
+let gameCounter = 100
+gameCounter++; // 100
+// console.log(gameCounter); // 101
+++gameCounter; //102
+// console.log(gameCounter); // 102
